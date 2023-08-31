dad46f90b894a23759d95fbc6f95726443973631
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-31 17:59:24 +0200

    Добавьте файлы проекта.

---
+++ BankProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
