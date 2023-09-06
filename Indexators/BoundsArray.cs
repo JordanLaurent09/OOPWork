77431e6e3f683c29313e6c4a1ce6b6104b7c7bdb
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-06 18:00:27 +0200

    indexes

---
+++ Indexators/BoundsArray.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexators
+{
+    class BoundsArray
+    {
+        int[] array;
+        public int Length;
+        public bool ErrorBounds;
+
+        public BoundsArray(int size)
+        {
+            array = new int[size];
+            Length = size;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if(index >=0 && index < Length)
+                {
+                    ErrorBounds = false;
+                    return array[index];
+                }
+                else
+                {
+                    ErrorBounds = true;
+                    return 0;
+                }
+            }
+
+            set
+            {
+                if(index >=0 && index < Length)
+                {
+                    ErrorBounds = false;
+                    array[index] = value;
+                }
+                else
+                {
+                    ErrorBounds = true;
+                }
+            }
+        }
+    }
+}
---
+++ Indexators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BoundsArray array = new BoundsArray(10);
+            array[0] = 12;
+            array[1] = 9;
+            Console.WriteLine(array.ErrorBounds);
+            array[13] = 11;
+            Console.WriteLine(array.ErrorBounds);
+
+            int x, y;
+            x = array[0];
+            Console.WriteLine(x);
+            Console.WriteLine(array.ErrorBounds);
+            y = array[11];
+            Console.WriteLine(y);
+            Console.WriteLine(array.ErrorBounds);
+        }
+    }
+}
