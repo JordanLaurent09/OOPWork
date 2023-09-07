a51c83434d70553a2bf12a1c541ce8b808dd385d
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-07 17:12:50 +0200

    inheritance

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -10,6 +10,27 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+            // Create instance of TwoDShape class and work with it fields and method 
+            TwoDShape shape = new TwoDShape();
+            shape.Height = 20;
+            shape.Width = 13;
+            shape.ShowDimension();
+
+            // Create instance of Triangle class and work with it fields and methods 
+            Triangle triangle = new Triangle();
+            triangle.Height = 21;
+            triangle.Width = 11;
+            triangle.Type = "Прямоугольный";
+            triangle.ShowDimension();
+            triangle.ShowType();
+            Console.WriteLine($"Triangle area is {triangle.Area()}");
+
+            Rectangle square = new Rectangle();
+            square.Height = 21;
+            square.Width = 21;
+            Console.WriteLine(square.IsSquare());
+            Console.WriteLine(square.Area());
+
         }
     }
 }
---
+++ Inheritance/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Rectangle : TwoDShape
+    {
+        // Checking whether rectagle is square or not 
+        public bool IsSquare()
+        {
+            if (Height == Width) return true;
+            return false;
+        }
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+    }
+}
