7e20e3a42fc950cd0f564b311c5129ce53193912
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-07 16:57:09 +0200

    inheritance

---
+++ Inheritance/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Triangle : TwoDShape
+    {
+        public string Type; // Type of the triangle
+
+
+        // Area of triangle
+        public double Area()
+        {
+            return Width * Height / 2;
+        }
+
+        public void ShowType()
+        {
+            Console.WriteLine($"Type of current triangle is {Type}");
+        }
+    }
+}
