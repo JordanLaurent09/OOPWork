7f576b283853f4a3c558f6583643e057b0acb4c3
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-07 16:51:11 +0200

    inheritance

---
+++ Inheritance/TwoDShape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class TwoDShape
+    {
+        public double Width;
+        public double Height;
+
+
+        public void ShowDimension()
+        {
+            Console.WriteLine($"Height and width equals {Height} and {Width}");
+        }
+    }
+}
