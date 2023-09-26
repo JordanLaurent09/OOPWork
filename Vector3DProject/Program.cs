e89faca50c6306d2ac9b643967555ed5cb601e1e
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-26 15:11:27 +0200

    3d vector

--- Vector3DProject/Program.cs
+++ Vector3DProject/Program.cs
@@ -10,6 +10,31 @@ namespace Vector3DProject
     {
         static void Main(string[] args)
         {
+            Vector3D one = new Vector3D(0.5, 0, 1);
+            Vector3D two = new Vector3D(0, 0.5, 1);
+
+            // Сумма векторов
+
+            Vector3D sum = one + two;
+            Console.WriteLine($"Координаты новоого вектора {sum.X},{sum.Y},{sum.Z}");
+
+            // Разность векторов
+
+            Vector3D sub = one - two;
+            Console.WriteLine($"Координаты новоого вектора {sub.X},{sub.Y},{sub.Z}");
+
+            // Скалярное произведение
+
+            Console.WriteLine(one * two);
+
+            // Длина вектора
+
+            Console.WriteLine(one.Vector3DLength());
+
+            // Косинус угла между векторами
+
+            Console.WriteLine(Vector3D.Cos(one, two));
+            Console.WriteLine(one.Cos(two));
         }
     }
 }
