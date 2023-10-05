60160fabbb07dfe92b065f1c98c0fb0182ade409
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-10-05 18:53:51 +0200

    delegate

---
+++ DelegateProject/Building.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProject
+{
+    class Building
+    {
+        public string BuildingName { get; set; }
+        public double Height { get; set; }
+        public int Floor { get; set; }
+        public double TotalArea { get; set; }
+        public string Location { get; set; }
+
+
+        public Building(string name, double height, int floor, double area, string location)
+        {
+            BuildingName = name;
+            Height = height;
+            Floor = floor;
+            TotalArea = area;
+            Location = location;
+        }
+
+        public static double CalculateVolume(double height, double areaTotal)
+        {
+            return height * areaTotal;
+        }
+
+
+    }
+}
---
+++ DelegateProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building[] buildings = new Building[3]{
+
+            new Building("House Garden", 6, 2, 172.2, "Garden"),
+            new Building("Business House", 48, 15, 2345.7, "City Center"),
+            new Building("Living House", 20, 5, 863.1, "Suburban area")
+
+            };
+
+
+            AreaDelegate areaDelegate = Building.CalculateVolume;
+            AreaInfo(buildings, areaDelegate);
+        }
+
+        public static void AreaInfo(Building[] buildings, AreaDelegate d)
+        {
+            foreach (Building item in buildings)
+            {
+                d = Building.CalculateVolume;
+                Console.WriteLine(d(item.Height, item.TotalArea));
+            }
+        }
+
+
+        public delegate double AreaDelegate(double height, double areaTotal);
+    }
+}
