90502eae68b45ee57fd72ffad4dbc3336f8b6e99
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-11-21 20:14:01 +0200

    clients list xlsx

---
+++ XLSXClientList/Program.cs
@@ -0,0 +1,23 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XLSXClientList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelPackage newBook = new ExcelPackage("Список клиентов.xlsx");
+            //ExcelWorksheet newWorkSheet = newBook.Workbook.Worksheets.Add("Клиенты");  // Лист создан, поэтому данная команда закомментирована
+            ExcelWorksheet newWorkSheet = newBook.Workbook.Worksheets["Клиенты"];
+
+
+
+            newBook.Save();
+        }
+    }
+}
