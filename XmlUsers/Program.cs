d1147eab4caf7197e75317dc60bd17038634c97b
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-12-01 13:11:29 +0200

    xml users

---
+++ XmlUsers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlUsers
+{
+    class Program
+    {
+        private static List<User> _users = new List<User>();
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+
+    }
+}
---
+++ XmlUsers/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlUsers
+{
+    class User
+    {
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, string password, int age)
+        {
+            Name = name;
+            Password = password;
+            Age = age;
+        }
+
+        public void CreateNewUser(List<User> users)
+        {
+            Console.WriteLine("Введите имя нового пользователя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите пароль нового пользователя");
+            string password = Console.ReadLine();
+            Console.WriteLine("Введите возраст нового пользователя");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (users.Count == 0)
+            {
+                User newUser = new User(name, password, age);
+                users.Add(newUser);
+                Console.WriteLine("Новый пользователь успешно добавлен");
+            }
+            else
+            {
+                bool isOccupied = false;
+
+
+                foreach (User user in users)
+                {
+                    if (user.Name == name || user.Password == password)
+                    {
+                        isOccupied = true;
+                    }
+                }
+
+                if (isOccupied == false)
+                {
+                    User newUser = new User(name, password, age);
+                    users.Add(newUser);
+                    Console.WriteLine("Новый пользователь успешно добавлен");
+                }
+                else Console.WriteLine("Такой пользователь уже есть");
+            }
+
+        }
+    }
+}
