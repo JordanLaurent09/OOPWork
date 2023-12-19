9eccf2c901dc5cc7ee97cc0d7b49184e114b2654
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-12-19 20:40:05 +0200

    word table average

--- WordTableAvArythmProject/Program.cs
+++ WordTableAvArythmProject/Program.cs
@@ -16,18 +16,21 @@ namespace WordTableAvArythmProject
 
             Random random = new Random();
 
+            // Начало работы с Word
             Word.Application wordApp = new Word.Application();
 
+            // Открытие документа
             Word.Document wordDoc = wordApp.Documents.Open($"{Directory.GetCurrentDirectory()}\\DigitsContainer.docx");
 
             Word.Range range = wordDoc.Content;
 
+            // Создание таблицы
             Word.Table table = wordDoc.Tables.Add(range, NumRows: 10, NumColumns: 10);
 
             table.Borders.OutsideLineStyle = Word.WdLineStyle.wdLineStyleSingle;
             table.Borders.InsideLineStyle = Word.WdLineStyle.wdLineStyleSingle;
 
-
+            // Заполнение таблицы случайными числами и вычисление среднего арифметического
             for (int i = 1; i <= 10; i++)
             {
                 for (int j = 1; j <= 10; j++)
@@ -44,6 +47,7 @@ namespace WordTableAvArythmProject
 
             Console.WriteLine($"Среднее арифметическое равно {numbersSum / numbersCount}");
 
+            // Сохранение изменений в файле и завершение работы
             wordDoc.Save();
             wordDoc.Close();
 
