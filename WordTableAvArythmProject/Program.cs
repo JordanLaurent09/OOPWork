1ccfdedc456f001288de7da7318b2685187adc0c
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-12-19 18:11:33 +0200

    word table average

---
+++ WordTableAvArythmProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WordTableAvArythmProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Word.Application wordApp = new Word.Application();
+
+            Word.Document wordDoc = wordApp.Documents.Open($"{Directory.GetCurrentDirectory()}\\DigitsContainer.docx");
+
+            Word.Range range = wordDoc.Content;
+
+            Word.Table table = wordDoc.Tables.Add(range, NumRows: 10, NumColumns: 10);
+
+            for(int i = 1; i <= 10; i++)
+            {
+                for(int j = 1; j <= 10; j++)
+                {
+                    table.Cell(i, j).Range.Text = random.Next(1, 21).ToString();
+                }
+            }
+
+            wordDoc.Save();
+            wordDoc.Close();
+
+            wordApp.Quit();
+        }
+    }
+}
