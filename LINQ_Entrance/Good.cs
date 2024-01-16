a76eadc068749ea2e9b799d49b75a284a68c79c4
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2024-01-16 21:54:31 +0200

    LINQ basics

---
+++ LINQ_Entrance/Good.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Entrance
+{
+    public class Good
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ LINQ_Entrance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using CsvHelper.Configuration;
+using CsvHelper;
+using System.Globalization;
+
+namespace LINQ_Entrance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Good> goods = new List<Good>();
+
+            using(StreamReader streamReader = new StreamReader("inventoryControl.csv"))
+            {
+                CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture);
+                CsvReader reader = new CsvReader(streamReader, config);
+
+                goods = reader.GetRecords<Good>().ToList();
+            }
+
+            IEnumerable<Good> evenGoods = goods.Where(p => p.Quantity % 2 == 0);
+
+            foreach(Good item in evenGoods)
+            {
+                Console.WriteLine($"Название товара: {item.Name}, количество: {item.Quantity}," +
+                    $"цена - {item.Price}, а вот описание: {item.Description}");
+            }
+        }
+    }
+}
